e85c855652c84d84e9de3afc539d7e05e8461d9c
kurotamashi30 <kurotamashi30@gmail.com>
2024-06-11 17:47:03 +0900

    test

--- first-project/main.js
+++ first-project/main.js
@@ -13,19 +13,23 @@ let userInput = document.getElementById("user-input");
 let resultArea = document.getElementById("result-area");
 let resetButton = document.getElementById("reset_button");
 let chances = 5;
+let gameOver = false;
+let chanceArea = document.getElementById("chansarea");
 //console.log(playButton);
 playButton.addEventListener("click", play); //("click",function)
 resetButton.addEventListener("click", reset);
 function pickRandomNum() {
   computerNum = Math.floor(Math.random() * 100) + 1; //Math.random()0から1の間(1が含まれてない)の数字をランダムで表しますMath.floorを使うことで自然数になります。
-  console.log("정답", computerNum);
+  console.log("正解", computerNum);
 }
 function play() {
   //user番号持ってくる値を持ってきます
   let userValue = userInput.value;
-  console.log(userValue);
   chances--;
-  console.log("chance", chance);
+  chanceArea.textContent = `残った機会${chances}回`;
+
+  console.log("chance", chances);
+
   if (userValue < computerNum) {
     console.log("UP!!");
     resultArea.textContent = "UP!!";
@@ -35,20 +39,22 @@ function play() {
   } else {
     console.log("BingGo!!");
     resultArea.textContent = "BingGo!!";
+    playButton.disabled = true;
   }
-  if (chances < 1) {
-    gameOver = ture;
-  }
-  if (gameOver == ture) {
-    playButton.disabled = ture;
+  if (chances <= 0) {
+    alert("ゲームオーバー！ すべての機会を使いました。");
+    playButton.disabled = true;
   }
 }
 function reset() {
   //userinputで内容綺麗に消す
   userInput.value = "";
   //新しい番号生成
+  chances = 5;
+  chanceArea.textContent = `残りの機会: ${chances} 回`;
   pickRandomNum();
-  resultArea.textContent = "結果の値がここに現れます。";
+  resultArea.textContent = "結果の値がここに表示されます。";
+  playButton.disabled = false;
 }
 
 pickRandomNum();
